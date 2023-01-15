c08450857b9d6a0b2b8fc98904970a2c181a2271
bunnyvarma <119424100+bunnyvarma@users.noreply.github.com>
2023-01-15 13:09:25 +0530

    Update 20230105120935-add-aaconstrain-in-vote.js

--- migrations/20230105120935-add-aaconstrain-in-vote.js
+++ migrations/20230105120935-add-aaconstrain-in-vote.js
@@ -3,12 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+   
     await queryInterface.addConstraint("Votes", {
       fields: ["electionId"],
       type: "foreign key",
@@ -36,11 +31,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    
   },
 };
